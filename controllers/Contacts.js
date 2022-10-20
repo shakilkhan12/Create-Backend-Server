c3bdbe35b319eaa9bfd4aaa1f066d603e65a22ae
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-20 22:29:45 +0500

    changes

--- controllers/Contacts.js
+++ controllers/Contacts.js
@@ -6,30 +6,46 @@ console.log(`${TOKEN}, ${VERSION}, ${LOCATION_ID}`);
 class Contacts {
   async createContact(req, res) {
     console.log(req.body);
+    const {
+      alpha_1,
+      alpha_2,
+      alpha_3,
+      alpha_4,
+      alpha_5,
+      alpha_6,
+      alpha_7,
+      alpha_8,
+    } = req.body;
 
-    // var options = {
-    //   method: "POST",
-    //   url: "https://api.msgsndr.com/contacts/",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Authorization: `Bearer ${TOKEN}`,
-    //     Version: VERSION,
-    //   },
-    //   data: {
-    //     name: "kamran",
-    //     email: "kamran@gmail.cm",
-    //     locationId: "ONLi33uXrYjMgH4oP0Qq",
-    //   },
-    // };
+    var options = {
+      method: "POST",
+      url: "https://api.msgsndr.com/contacts/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        Version: VERSION,
+      },
+      data: {
+        firstName: alpha_1,
+        lastName: alpha_2,
+        name: alpha_3,
+        email: alpha_4,
+        locationId: LOCATION_ID,
+        phone: alpha_5,
+        address1: alpha_6,
+        city: alpha_7,
+        website: alpha_8,
+      },
+    };
 
-    // axios
-    //   .request(options)
-    //   .then(function (response) {
-    //     console.log(response.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
   async getData(req, res) {
     try {
