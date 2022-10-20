330f8675a1e577b9aed650a2ed066fa0669f7c10
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-20 15:09:23 +0500

    some changes

--- controllers/Contacts.js
+++ controllers/Contacts.js
@@ -5,22 +5,26 @@ const LOCATION_ID = process.env.LOCATION_ID;
 class Contacts {
   async createContact(req, res) {
     console.log(req.body);
-    const response = await axios.post(
-      "https://api.msgsndr.com/contacts/",
-      {
-        name: req.body.alpha_1,
-        email: req.body.alpha_2,
-        locationId: LOCATION_ID,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${TOKEN}`,
-          Version: VERSION,
+    try {
+      const response = await axios.post(
+        "https://api.msgsndr.com/contacts/",
+        {
+          name: req.body.alpha_1,
+          email: req.body.alpha_2,
+          locationId: LOCATION_ID,
         },
-      }
-    );
-    res.json({ data: req.body, response });
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${TOKEN}`,
+            Version: VERSION,
+          },
+        }
+      );
+      res.json({ data: req.body, response });
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
 module.exports = new Contacts();
