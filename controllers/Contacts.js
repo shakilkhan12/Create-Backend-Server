fb6ba36b91c8d182e54c40722e928c9056cdb170
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-20 22:18:23 +0500

    some changes

--- controllers/Contacts.js
+++ controllers/Contacts.js
@@ -6,23 +6,53 @@ console.log(`${TOKEN}, ${VERSION}, ${LOCATION_ID}`);
 class Contacts {
   async createContact(req, res) {
     console.log(req.body);
+
+    // var options = {
+    //   method: "POST",
+    //   url: "https://api.msgsndr.com/contacts/",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${TOKEN}`,
+    //     Version: VERSION,
+    //   },
+    //   data: {
+    //     name: "kamran",
+    //     email: "kamran@gmail.cm",
+    //     locationId: "ONLi33uXrYjMgH4oP0Qq",
+    //   },
+    // };
+
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     console.log(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+  }
+  async getData(req, res) {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  async createPost(req, res) {
     try {
       const response = await axios.post(
-        "https://api.msgsndr.com/contacts/",
+        "https://jsonplaceholder.typicode.com/todos",
         {
-          name: req.body.alpha_1,
-          email: req.body.alpha_2,
-          locationId: LOCATION_ID,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${TOKEN}`,
-            Version: VERSION,
-          },
+          userId: 1200,
+          id: 12000,
+          title: "my todo title",
+          completed: true,
         }
       );
-      return res.json({ response });
+      console.log(response);
     } catch (error) {
       console.log(error.message);
     }
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,4 +2,6 @@ const { Router } = require("express");
 const Contacts = require("../controllers/Contacts");
 const router = Router();
 router.post("/create", Contacts.createContact);
+router.get("/users", Contacts.getData);
+router.post("/create-post", Contacts.createPost);
 module.exports = router;
