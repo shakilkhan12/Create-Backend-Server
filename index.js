590f2b8ddcf0ce9e396894a0ebb4f2a3de1eaf77
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-20 12:49:03 +0500

    added get route

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 app.post("/create", (req, res) => {
   res.json({ data: req.body });
 });
+app.get("/test", (req, res) => {
+  res.json("Hello");
+});
 app.listen(5000, () => {
   console.log("Server is running");
 });
