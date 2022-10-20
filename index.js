09876a9e3bf5bcdec1d9df979b8c6f7514013dad
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-20 13:09:09 +0500

    added dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.post("/create", (req, res) => {
   res.json({ data: req.body });
 });
 app.get("/test", (req, res) => {
   res.json("Hello");
 });
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server is running");
 });
