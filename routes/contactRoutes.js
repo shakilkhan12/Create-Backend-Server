d4681b412b340bc6318cee0e10541fe845b81dd6
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-21 10:57:31 +0500

    changes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,6 +2,4 @@ const { Router } = require("express");
 const Contacts = require("../controllers/Contacts");
 const router = Router();
 router.post("/create", Contacts.createContact);
-router.get("/users", Contacts.getData);
-router.post("/create-post", Contacts.createPost);
 module.exports = router;
