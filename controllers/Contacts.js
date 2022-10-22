749f546d411dde1f3c4dd68ec0d6081423667c30
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-22 08:11:05 +0500

    changes

--- controllers/Contacts.js
+++ controllers/Contacts.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
 const TOKEN = process.env.ACCESS_TOKEN;
 const VERSION = process.env.VERSION;
 const LOCATION_ID = process.env.LOCATION_ID;
 console.log(`${TOKEN}, ${VERSION}, ${LOCATION_ID}`);
 class Contacts {
   async createContact(req, res) {
-    console.log(req.body.formMetaData);
-    const { formMetaData } = req.body;
+    console.log(req.body);
+    // const { formMetaData } = req.body;
     // const {
     //   alpha_1,
     //   alpha_2,
@@ -18,28 +19,43 @@ class Contacts {
     //   alpha_8,
     // } = req.body;
 
-    var options = {
-      method: "POST",
-      url: "https://api.msgsndr.com/contacts/",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${TOKEN}`,
-        Version: VERSION,
-      },
-      data: {
-        locationId: LOCATION_ID,
-        customFields: formMetaData,
-      },
-    };
+    // let options = {
+    //   method: "POST",
+    //   url: "https://api.msgsndr.com/contacts/",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${TOKEN}`,
+    //     Version: VERSION,
+    //   },
+    //   data: {
+    //     locationId: LOCATION_ID,
+    //     firstName: "zahid",
+    //     lastName: "zaman",
+    //     name: "zahid zaman",
+    //     email: "zahidzaman@deos.com",
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    //     customFields: [
+    //       {
+    //         id: uuidv4(),
+    //         field_value: "some custom field value",
+    //       },
+    //       {
+    //         id: uuidv4(),
+    //         field_value: "shakil khan custom",
+    //       },
+    //     ],
+    //   },
+    // };
+
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     console.log(response.data);
+    //     res.send(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
   }
 }
 module.exports = new Contacts();
